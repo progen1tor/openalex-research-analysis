e8e105c8633311a0879c1226ef91db7843749a30
ywz knnd <akmn140@gmail.com>
2026-07-08 15:20:23 +0300

    Fix path in constants

--- src/constants.py
+++ src/constants.py
@@ -2,4 +2,4 @@ from fake_useragent import UserAgent
 
 START_URL = 'https://api.openalex.org'
 USER_AGENT = UserAgent().random
-LOG_FILENAME = 'parser_errors.log'
+LOG_FILENAME = 'openalex-research-analysis/parser_errors.log'
