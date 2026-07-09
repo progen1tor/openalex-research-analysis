00447089ef1f79b4163417d1834fae6894433d0d
ywz knnd <akmn140@gmail.com>
2026-07-09 16:41:35 +0300

    Add filename for raw data

--- src/constants.py
+++ src/constants.py
@@ -3,3 +3,4 @@ from fake_useragent import UserAgent
 START_URL = 'https://api.openalex.org'
 USER_AGENT = UserAgent().random
 LOG_FILENAME = 'openalex-research-analysis/parser_errors.log'
+RAW_DATA_FILENAME = 'openalex-research-analysis/data/raw.json'
