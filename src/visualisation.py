f11c8be9f539602bf676caacefeec2ab1a44fc2e
ywz knnd <akmn140@gmail.com>
2026-07-14 15:17:09 +0300

    Fix & improve visualisation

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,40 +1,109 @@
 import matplotlib.pyplot as plt 
 import seaborn as sns 
 import pandas as pd 
-from processing import openalex_dataframe
 from constants import GRAPHS_PATH
 from analysis import number_of_publications_by_year, open_access_statistics, publication_types, most_popular_themes
 
 def number_of_publications_by_year_graph(df: pd.DataFrame) -> None: 
-    data = number_of_publications_by_year(df)
-    sns.lineplot(data, color='red')  
+    data = number_of_publications_by_year(df).reset_index()
+    
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(
+        data, 
+        x='publication_year', 
+        y='publication_count', 
+        color='steelblue'
+        ) 
+    
     plt.title('Number of Publications by Year')
-    plt.savefig(f'{GRAPHS_PATH}number_of_publications_by_year_graph.png', bbox_inches='tight')
+    plt.xlabel('Publication year')
+    plt.grid(alpha=.3)
+    
+    plt.savefig(
+        f'{GRAPHS_PATH}number_of_publications_by_year_graph.png', bbox_inches='tight',
+        dpi=300
+        )
+    plt.close()
+    
     
 def open_access_statistics_graph(df: pd.DataFrame) -> None: 
-    data = open_access_statistics(df)
+    data = open_access_statistics(df).reset_index()
+    
     plt.figure(figsize=(15, 8))
-    sns.lineplot(data, color='red', lw=2)  
+    sns.lineplot(
+        data, 
+        x='publication_year', 
+        y='open_access_pct', 
+        color='#3366CC', 
+        lw=2
+        )  
+    
     plt.title('Open Access Percentage by Year')
-    plt.xticks(data[::4].index.to_list() + [2026])  
-    plt.savefig(f'{GRAPHS_PATH}open_access_statistics_graph.png', bbox_inches='tight')  
+    plt.xlabel('Publication year')
+    plt.ylabel('Open Access (%)')
+    plt.grid(alpha=.3)
+    
+    plt.savefig(
+        f'{GRAPHS_PATH}open_access_statistics_graph.png', 
+        bbox_inches='tight',
+        dpi=300
+        )
+    plt.close()  
+    
     
 def publication_types_graph(df: pd.DataFrame) -> None: 
-    data = publication_types(df).sum().sort_values(ascending=False)
-    data_pt_1 = data.iloc[:3]
-    data_pt_2 = data.iloc[3:]
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
-    gr1 = sns.barplot(data_pt_1, ax=axes[0], palette=['#141173', '#7B1E7A', '#F9564F'])
-    gr2 = sns.barplot(data_pt_2, ax=axes[1], palette=['#B33F62', "#FF9E81", '#F3C677', "#FF4F4F", '#F18805'])
-    plt.suptitle('Number of Publication by Type', y=.94, fontsize=20)
-    gr1.set_xlabel('type', fontsize=13.5)
-    gr2.set_xlabel('type', fontsize=13.5)
-    gr1.set_xticklabels(data_pt_1.index.to_list(), fontsize=12)
-    gr2.set_xticklabels(data_pt_2.index.to_list(), fontsize=12)
-    plt.savefig(f'{GRAPHS_PATH}publication_types_graph.png', bbox_inches='tight')
+    data = publication_types(df)
+    data_pt_1 = data.loc[data.publication_count >= 50].reset_index()
+    data_pt_2 = data.loc[data.publication_count < 50].reset_index()
+    
+    _, axes = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
+    sns.barplot(
+        data_pt_1, 
+        x='type', 
+        y='publication_count', 
+        ax=axes[0], palette=['#264653','#2A9D8F','#E9C46A'],
+        hue='type'
+        )
+    sns.barplot(
+        data_pt_2, 
+        x='type', 
+        y='publication_count', 
+        ax=axes[1], 
+        palette=['#F4A261','#E76F51','#457B9D','#1D3557','#A8DADC'],
+        hue='type'
+        )
+    
+    plt.suptitle('Number of Publications by Type', y=.94, fontsize=20)
+    axes[0].set_xlabel('Type', fontsize=13.5)
+    axes[1].set_xlabel('Type', fontsize=13.5)
+    axes[0].tick_params(axis='x', labelsize=12)  
+    axes[1].tick_params(axis='x', labelsize=12)
+    
+    plt.savefig(
+        f'{GRAPHS_PATH}publication_types_graph.png', 
+        bbox_inches='tight',
+        dpi=300
+        )
+    plt.close()
+    
     
 def most_popular_themes_graph(df: pd.DataFrame) -> None: 
-    data = most_popular_themes(df).iloc[:10].reset_index()  
-    sns.barplot(data, y='primary_topic', x='publication_count', hue='primary_topic', palette='viridis')
-    plt.suptitle('Top 10 Popular Topics', x=.2, y=.94, fontsize=14)
-    plt.savefig(f'{GRAPHS_PATH}most_popular_themes_graph.png')
+    data = most_popular_themes(df).iloc[:10].reset_index() 
+    plt.figure(figsize=(10, 6))
+    sns.barplot(
+        data,
+        y='primary_topic',
+        x='publication_count',
+        hue='primary_topic',  
+        palette='viridis',
+    ) 
+    
+    plt.suptitle('Top 10 Popular Topics', x=.32, y=.94, fontsize=14)
+    plt.xlabel('Number of Publications')
+    plt.ylabel('')
+    plt.savefig(
+        f'{GRAPHS_PATH}most_popular_themes_graph.png', 
+        bbox_inches='tight',
+        dpi=300
+        )
+    plt.close()
